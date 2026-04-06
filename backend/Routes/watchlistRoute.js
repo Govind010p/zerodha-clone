d6b9e25e16a2fe02284ad9afba6b877334a4c030
Govind010p <govindprajapat9794@gmail.com>
2026-04-06 20:47:13 +0530

    fix: cross-domain cookies, socket transports, keep-alive

--- backend/Routes/watchlistRoute.js
+++ backend/Routes/watchlistRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../Middleware/Auth");
 
 const { getWatchlistData } = require("../Controllers/watchlistController");
 
