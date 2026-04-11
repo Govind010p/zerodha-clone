371074ef1d8a13f7e7d77f965fb9ea9f5e2cbd91
Govind010p <govindprajapat9794@gmail.com>
2026-04-11 14:48:23 +0530

    now all fixed 4

--- frontend/src/deshboard_page/WatchList.jsx
+++ frontend/src/deshboard_page/WatchList.jsx
@@ -6,10 +6,13 @@ import { watchlist } from "../Data/data";
 import WatchlistItem from "./WatchlistItem";
 // import socket from "../socket.js";
 // import axios from "axios";
-import GeneralContext  from "./GeneralContext";
+import GeneralContext from "./GeneralContext";
 
 function WatchList() {
-  const {stockData  } = useContext(GeneralContext);
+  const { stockData } = useContext(GeneralContext);
+
+  const stocks = stockData.filter((item) => item.type === "stock");
+  const crypto = stockData.filter((item) => item.type === "crypto");
 
   return (
     <div className="container row p-0">
@@ -33,11 +36,28 @@ function WatchList() {
       </div>
       <div className="div border page-content-watchlist"></div>
       <div className="mt-3 p-md-0 ps-2">
-        <ul className="watchlist">
-          {stockData.map((stock, index) => {
-            return <WatchlistItem stock={stock} key={index} />;
-          })}
-        </ul>
+        <div className="mt-3 p-md-0 ps-2">
+          {stocks.length > 0 && (
+            <>
+              <div className="badge bg-primary mb-2 ms-2">Stocks</div>
+              <ul className="watchlist">
+                {stocks.map((stock, index) => (
+                  <WatchlistItem stock={stock} key={`stock-${index}`} />
+                ))}
+              </ul>
+            </>
+          )}
+          {crypto.length > 0 && (
+            <>
+              <div className="badge bg-warning text-dark mb-2 ms-2">Crypto</div>
+              <ul className="watchlist">
+                {crypto.map((stock, index) => (
+                  <WatchlistItem stock={stock} key={`crypto-${index}`} />
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
